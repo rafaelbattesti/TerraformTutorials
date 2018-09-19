ae780147d910750e911c4a9400300c991cbad812
rafaelbattesti <rafael.battesti@gmail.com>
2018-09-18 20:30:58 -0400

    Add security group to allow ICMP.

--- 1-stage/main.tf
+++ 1-stage/main.tf
@@ -6,14 +6,29 @@ provider "aws" {
     secret_key = "${var.aws_secret}"
 }
 
+resource "aws_security_group" "allow-icmp" {
+  name        = "allow-icmp"
+  description = "Allow icmp inbound traffic - ping"
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "Allow icmp inbound traffic"
+  }
+}
 resource "aws_instance" "web" {
     ami                    = "ami-0ef6235c4ed871e29" # Ubuntu Trusty 14.04 amd64 hvm:ebs
     instance_type          = "t2.micro"
     subnet_id              = "subnet-04b5f928"
-    vpc_security_group_ids = ["sg-948305e4"]
+    vpc_security_group_ids = ["${aws_security_group.allow-icmp.id}"]
 
     tags {
-        "Identity" = "rbattesti.test.terraform"
+        "Identity" = "rbattesti-test-terraform"
         "Name"     = "RB Terraform Test"
     }
 }
