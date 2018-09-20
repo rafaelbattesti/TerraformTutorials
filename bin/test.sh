c1b78180d0148b44b6fd81143d4d5acae80d3afb
rafaelbattesti <rafael.battesti@gmail.com>
2018-09-19 23:49:41 -0400

    Validating test script.

--- bin/test.sh
+++ bin/test.sh
@@ -2,8 +2,19 @@
 
 set +e
 
+echo <<EOF
+
+=========================
+Infrastructure Validation
+=========================
+
+EOF
+
+
 # Output all results, but fail build if one test fails
-declare -i fail=0
+declare -i fail_build=0
+declare -i total=0
+declare -i failed=0
 
 # Output colors
 red=$(tput setaf 1)
@@ -11,41 +22,34 @@ green=$(tput setaf 2)
 reset=$(tput sgr0)
 
 # 1. Test public IP
+test_id="001 : Ping Public IP"
 echo "Running ICMP test for Public IP: $(terraform output public_ip)"
-((count = 10))
-while [[ $count -ne 0 ]] ; do
-    ping -c 1 $(terraform output public_ip)
-    rc=$?
-    if [[ $rc -eq 0 ]] ; then
-        ((count = 1))
-        echo "${green}PASS${reset}"
-    fi
-    ((count = count - 1))
-done
-
-if [[ $rc -ne 0 ]] ; then
-    echo "${red}FAIL${reset}"
-    fail=1
+ping -c 5 $(terraform output public_ip) > /dev/null 2>&1
+rc=$?
+if [[ $rc -eq 0 ]] ; then
+    echo "${green}test_id${reset}"
+else
+    echo "${red}test_id${reset}"
+    ((failed = failed + 1))
+    fail_build=1
 fi
 
 # 2. Test public DNS
+test_id="002 : Ping Public DNS"
 echo "Running ICMP test for Public DNS: $(terraform output public_dns)"
-((count = 10))
-while [[ $count -ne 0 ]] ; do
-    ping -c 1 $(terraform output public_dns)
-    rc=$?
-    if [[ $rc -eq 0 ]] ; then
-        ((count = 1))
-        echo "${green}PASS${reset}"
-    fi
-    ((count = count - 1))
-done
-
-if [[ $rc -ne 0 ]] ; then
-    echo "${red}FAIL${reset}"
-    fail=1
+ping -c 5 $(terraform output public_dns) > /dev/null 2>&1
+rc=$?
+if [[ $rc -eq 0 ]] ; then
+    echo "${green}test_id${reset}"
+else
+    echo "${red}test_id${reset}"
+    ((failed = failed + 1))
+    fail_build=1
 fi
 
-if [[ fail -ne 0 ]] ; then
+echo "total: ${total}, failed: ${failed}"
+
+if [[ fail_build -ne 0 ]] ; then
+    echo "${red}Build Failed${reset}"
     exit 1
 fi
