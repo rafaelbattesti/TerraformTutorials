d87ac07170952b7c74785b7c58306e8ad0385d97
rafaelbattesti <rafael.battesti@gmail.com>
2018-09-20 00:11:07 -0400

    See build fail again.

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_instance" "web" {
     ami                    = "ami-0ef6235c4ed871e29" # Ubuntu Trusty 14.04 amd64 hvm:ebs
     instance_type          = "t2.micro"
     subnet_id              = "subnet-04b5f928"
-    vpc_security_group_ids = ["${aws_security_group.allow-icmp.id}"]
+    vpc_security_group_ids = ["sg-948305e4"]
 
     tags {
         "Identity" = "rbattesti-test-terraform"
