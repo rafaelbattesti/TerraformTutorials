53e9190cc396eeaf39e96ba7bf66a6572b670652
rafaelbattesti <rafael.battesti@gmail.com>
2018-10-29 14:22:53 -0400

    Change instance size to small

--- main.tf
+++ main.tf
@@ -23,13 +23,13 @@ resource "aws_security_group" "allow-icmp" {
 }
 resource "aws_instance" "web" {
     ami                    = "ami-0ef6235c4ed871e29" # Ubuntu Trusty 14.04 amd64 hvm:ebs
-    instance_type          = "t2.micro"
+    instance_type          = "t2.small"
     subnet_id              = "subnet-04b5f928"
     vpc_security_group_ids = ["${aws_security_group.allow-icmp.id}"] # Use sg-948305e4 for failed Demo
 
     tags {
-        "Identity" = "rbattesti-test-terraform"
-        "Name"     = "RBattesti Git-Terraform-TravisCI"
+        "Identity" = "rbattesti-demoapp"
+        "Name"     = "RBattesti DemoApp"
     }
 }
 
